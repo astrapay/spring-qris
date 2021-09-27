febb3eb38948d5a3d04c89aed1ea7014986de6e6
Gilbert Subay <gilbertttsubay@gmail.com>
2021-09-27 08:54:12 +0700

    add nns

--- src/main/java/com/astrapay/qris/QrisNationalNumberingSystem.java
+++ src/main/java/com/astrapay/qris/QrisNationalNumberingSystem.java
@@ -13,7 +13,14 @@ public interface QrisNationalNumberingSystem {
         BLUEPAY(93600919, Switching.ALTO),
         DOKU(93600899, Switching.ALTO),
         ASTRAPAY(93600822, Switching.ALTO),
+        BANK_TABUNGAN_NEGARA(93600200,Switching.ALTO),
+        DIGITAL_BCA(93600501,Switching.ALTO),
+
+
+
+
         BNI(93600009, Switching.ARTAJASA),
+        IMKAS(93600789, Switching.ARTAJASA),
         PERMATA(93600013, Switching.ARTAJASA),
         BPD_JATIM(93600114, Switching.ARTAJASA),
         BPD_BALI(93600129, Switching.ARTAJASA),
@@ -32,8 +39,17 @@ public interface QrisNationalNumberingSystem {
         BANK_QNB(93600167, Switching.ARTAJASA),
         BANK_COMMONWEALTH(93600950, Switching.ARTAJASA),
         BANK_MAYORA(93600553, Switching.ARTAJASA),
+        BPD_PAPUA(93600132,Switching.ARTAJASA),
+        BANK_JABAR_BANTEN_SYARIAH(93600425,Switching.ARTAJASA),
+
+
+        SINARMAS_RINTIS(93600153,Switching.RINTIS),
+        KB_BUKOPIN(93600441,Switching.RINTIS),
+        BPD_JATIM_RINTIS(93600114,Switching.RINTIS),
         DANAMON(93600011, Switching.RINTIS),
+        BANK_RIAU_KEPRI(93600119,Switching.RINTIS),
         BCA(93600014, Switching.RINTIS),
+        BCA_SYARIAH(93600536, Switching.RINTIS),
         CIMB(93600022, Switching.RINTIS),
         OCBC(93600028, Switching.RINTIS),
         BANK_JABAR(93600110, Switching.RINTIS),
@@ -42,6 +58,7 @@ public interface QrisNationalNumberingSystem {
         BANK_MEGA(93600426, Switching.RINTIS),
         OTTOCASH(93600811, Switching.RINTIS),
         BPD_SUMSEL_BABEL(93600120, Switching.RINTIS),
+        MULTIARTA_SENTOSA(93600548,Switching.RINTIS),
         SULSELBAR(93600126, Switching.RINTIS),
         BTPN(93600213, Switching.RINTIS),
         HANA(93600484, Switching.RINTIS),
@@ -50,6 +67,12 @@ public interface QrisNationalNumberingSystem {
         BPD_DIY(93600112, Switching.RINTIS),
         BANK_SAHABAT_SAMPOERNA(93600523, Switching.RINTIS),
         BANK_ARTHA_GRAHA_INTERNATIONAL(93600037, Switching.RINTIS),
+        BUMI_ARTA(93600076,Switching.RINTIS),
+
+
+
+        BANK_ACEH_SYARIAH(93600116,Switching.JALIN),
+        BPD_NTT_JALIN(93600130,Switching.JALIN),
         BRI(93600002, Switching.JALIN),
         MANDIRI(93600008, Switching.JALIN),
         BIMASAKTI(93600815, Switching.JALIN),
@@ -59,7 +82,10 @@ public interface QrisNationalNumberingSystem {
         NETZME(93600814, Switching.JALIN),
         MNC_SPIN(93600816, Switching.JALIN),
         PAYDIA(93600818, Switching.JALIN),
-        YUKK(93600817, Switching.JALIN);
+        YUKK(93600817, Switching.JALIN),
+        BAYARIND(93600808,Switching.JALIN),
+        POS_INDONESIA(93608161,Switching.JALIN),
+        GPAY(93600813,Switching.JALIN);
 
         private Integer code;
         private Switching switching;
