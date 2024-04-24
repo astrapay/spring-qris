78074b2c7d6d2c778bf468610214f7586caf8aea
ap-sendy-agustian <sendy.agustian@astrapay.com>
2024-04-24 16:19:10 +0700

    Update QrCpmPayload.java

--- src/main/java/com/astrapay/qris/cpm/object/QrCpmPayload.java
+++ src/main/java/com/astrapay/qris/cpm/object/QrCpmPayload.java
@@ -1,5 +1,20 @@
 package com.astrapay.qris.cpm.object;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class QrCpmPayload {
     // object to represent payload (unparsed, string format) and do validations
+    private String payloadBase64;
+    private String payloadHex;
+    private Map<String, QrCpmDataObject> qrisRoot;
 }
